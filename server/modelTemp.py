70774490dc6b1d5ae139d7a33914629b78b9e3cb
PandaOnTheFly <74941296+andyjianzhou@users.noreply.github.com>
2023-02-18 14:53:57 -0700

    Created model temp.py
    Created model file for ML

---
+++ server/modelTemp.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn import svm
+import joblib
+
+from sklearn.metrics import r2_score
+def compare_models(model):
+    model_name = model.__class__.__name__
+    fit=model.fit(train_data,train_labels)
+    y_pred=fit.predict(test_data)
+    # save model weights
+    joblib.dump(fit, f'model{model_name}.pkl')
+            
+    r2=r2_score(test_labels,y_pred)
+    return([model_name,r2])
+
+
+models = [
+    GradientBoostingRegressor(n_estimators=200, max_depth=3, random_state=0),
+     RandomForestRegressor(n_estimators=200, max_depth=3, random_state=0),
+    svm.SVR(),
+   DecisionTreeRegressor()
+]
+
+model_train=list(map(compare_models,models)) 
+
+print(*model_train, sep = "\n")
+
+# deploy model
+model.load(joblibFile)
