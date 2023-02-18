fe412e4dc509d3718f2cbc765b21aa04ba2de804
Farel Lukas <farellukas@gmail.com>
2023-02-18 14:54:08 -0700

    Implemented navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,12 @@
-function App() {}
+import React from "react";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
 
 export default App;
