2f0003c0c04ecea7e1f6840f38cda71a5f86c03d
Paarthbathla338 <Paarth.bathla@outlook.com>
2023-02-18 17:40:22 -0700

    Globe Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainPage from "./routes/MainPage";
+import Globe from "./routes/Globe";
 
 // Add all routes here
 const router = createBrowserRouter([
   {
     path: "/",
     element:<MainPage/>
+  },
+
+  {
+    path: "/main",
+    element:<Globe/>
   },
 ]);
 
