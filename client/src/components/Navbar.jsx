609cd7d93e6cf18e0a5380fe3d7387146936d2d7
Farel Lukas <farellukas@gmail.com>
2023-02-19 08:20:43 -0700

    Added links to nav items

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="w-full absolute bg-[#18181B]">
+    <nav className="w-full absolute z-50">
       <div className="flex py-6 max-w-screen-xl w-[90%] mx-auto items-center lg:gap-32 gap-4 lg:justify-start justify-evenly flex-col lg:flex-row">
         {/* Logo */}
         <Link to="/">
@@ -17,12 +17,20 @@ function Navbar() {
               Home
             </p>
           </Link>
-          <p className="opacity-75 hover:opacity-100 transition-opacity duration-300">
-            How it works
-          </p>
-          <p className="opacity-75 hover:opacity-100 transition-opacity duration-300">
-            GitHub
-          </p>
+          <a href="" target="_blank" rel="noopener">
+            <p className="opacity-75 hover:opacity-100 transition-opacity duration-300">
+              How it works
+            </p>
+          </a>
+          <a
+            href="https://github.com/andyjianzhou/CalgaryHacks2023"
+            target="_blank"
+            rel="noopener"
+          >
+            <p className="opacity-75 hover:opacity-100 transition-opacity duration-300">
+              GitHub
+            </p>
+          </a>
         </div>
       </div>
     </nav>
